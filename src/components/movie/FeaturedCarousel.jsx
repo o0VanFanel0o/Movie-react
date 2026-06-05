d3b225f6aaa32e1d5a4702bc7d28ca9ab0ab55dc
Luis Guevara <199848867+o0VanFanel0o@users.noreply.github.com>
2026-06-05 14:04:48 -0600

    Add interactive featured carousel

--- src/components/movie/FeaturedCarousel.jsx
+++ src/components/movie/FeaturedCarousel.jsx
@@ -1,9 +1,58 @@
+import { useEffect, useState } from "react"
+import {getTrendingMovies} from "../../services/api"
+import "../../styles/FeaturedCarousel.css"
+
 const FeaturedCarousel = () => {
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    useEffect(() => {
+        const fetchMovies = async() => {
+            try{
+                setLoading(true)
+                setError("")
+                const trendingMovies = await getTrendingMovies()
+                setMovies(trendingMovies)
+            }catch(error) {
+                console.error("Error loading featured movies: ",error)
+                setError("Something went wrong while loading featured movies")
+            }finally {
+                setLoading(false)
+            }
+        }
+        fetchMovies()
+    }, [])
+    if(loading) {
+        return <p>Loading featured movies</p>
+    }
+    if(error) {
+        return <p>{error}</p>
+    }
+    const activeMovie = movies[activeIndex]
     return (
         <section className="featured-carousel">
-            <p>Featured Carousel</p>
+            <div className="featured-track">
+                {movies.map((movie, index) => (
+                    <article className={`featured-card ${index === activeIndex ? "active" : ""}`} key={movie.id}
+                        onClick={() => setActiveIndex(index)}>
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                        alt={movie.title} />
+
+                    </article>
+                ))}
+            </div>
+            {activeMovie && (
+                <div className="featured-info">
+                    <h2>{activeMovie.title}</h2>
+                    <p>{activeMovie.vote_average?.toFixed(1)|| "N/A"}</p>
+                    <p>{activeMovie.overview || "No overview avalable."}</p>
+                </div>
+            )}
         </section>
     )
 }
+
 
 export default FeaturedCarousel
